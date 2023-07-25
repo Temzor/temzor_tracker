03fc188c2b31067d5be897e34818da0c91f1b417
d.kapustin <onewaykd@gmail.com>
2023-07-25 17:38:49 +0300

    1.3. Вызов метода с аргументами. ADD TEST

---
+++ src/test/java/ru/job4j/oop/JukeboxTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JukeboxTest {
+    @Test
+    void hybridTestJukebox() {
+        Jukebox jukebox = new Jukebox();
+        assertThat(jukebox).isNotNull();
+        assertThat(jukebox).isEqualTo("Песня не найдена");
+    }
+
+}
