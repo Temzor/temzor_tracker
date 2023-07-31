4fb5614dbe7e51862ed22cf936e175276bbc817c
d.kapustin <onewaykd@gmail.com>
2023-07-31 18:03:32 +0300

    4. Расстояние между точками в трехмерном пространстве.

--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 class PointTest {
     @Test
-    void whenPositivePoint() {
+    void whenPositivePoint2D() {
         Point a = new Point(1, 18);
         Point b = new Point(12, 25);
         assertThat(a.distance(b)).isEqualTo(13, withPrecision(2d));
@@ -14,9 +14,24 @@ class PointTest {
     }
 
     @Test
-    void whenNegativePoint() {
+    void whenNegativePoint2D() {
         Point a = new Point(-1, -20);
         Point b = new Point(-32, -58);
+        assertThat(a.distance(b)).isEqualTo(49, withPrecision(2d));
+    }
+
+    @Test
+    void whenPositivePoint3D() {
+        Point a = new Point(1, 18, 22);
+        Point b = new Point(12, 25, 31);
+        assertThat(a.distance3d(b)).isEqualTo(15, withPrecision(2d));
+
+    }
+
+    @Test
+    void whenNegativePoint3D() {
+        Point a = new Point(-1, -20, -45);
+        Point b = new Point(-32, -58, -89);
         assertThat(a.distance(b)).isEqualTo(49, withPrecision(2d));
     }
 
