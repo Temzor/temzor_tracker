8d4f96d186cff2112b43c5e3e69bf303ac201bc2
d.kapustin <onewaykd@gmail.com>
2023-08-01 13:57:43 +0300

    6.2. Локальные и анонимные внутренние классы

---
+++ src/main/java/ru/job4j/oop/inner/OuterClass.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop.inner;
+
+public class OuterClass {
+    class NestedClass {
+
+    }
+
+    static class StaticNestedClass {
+
+    }
+}
---
+++ src/main/java/ru/job4j/oop/local/Local.java
@@ -0,0 +1,23 @@
+package ru.job4j.oop.local;
+
+public class Local {
+    private String name = "Dmitrii";
+
+    public void getFullName() {
+        final String surname = "Kapustin";
+
+        class FullName {
+            public void printFullName() {
+                System.out.println(name + " " + surname);
+            }
+        }
+        FullName fullName = new FullName();
+        fullName.printFullName();
+    }
+
+    public static void main(String[] args) {
+        Local local = new Local();
+        local.getFullName();
+    }
+
+}
